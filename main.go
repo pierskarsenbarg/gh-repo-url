3ea597b358c74b643e8ab8a54616af90446d1632
Piers Karsenbarg <piers@pulumi.com>
2026-02-26 08:14:04 +0000

    First commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cli/go-gh/v2/pkg/repository"
+)
+
+func main() {
+	repo, err := repository.Current()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("https://%s/%s/%s\n", repo.Host, repo.Owner, repo.Name)
+	}
+}
